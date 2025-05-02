fea39302b9d55ab9fc74e0ee23ec9d89aa29a4a8
Alano Arraes <alanoarraes@gmail.com>
2025-05-02 16:09:41 -0400

    commit regular

--- sistema-smart-tv/sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/sistema-smart-tv/src/Usuario.java
@@ -1,5 +1,25 @@
+
+import java.util.Scanner;
+
+
+
 public class Usuario {
     public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+        System.out.println("Nome: " + nome + " " + sobrenome);
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.println("Idade: " + idade);
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+        System.out.println("Altura: " + altura);
+        
+        
         SmartTv smartTv = new SmartTv();
         System.out.println("A TV está ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canalAtual);
