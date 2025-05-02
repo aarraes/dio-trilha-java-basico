d6661abac480fe087864d14b32cdfb337f084282
Alano Arraes <alanoarraes@gmail.com>
2025-05-02 16:16:29 -0400

    Commit basico

--- conta-banco/conta-banco/src/ContaTerminal.java
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe scanner
+        
+
+        //Exibir as mensagens de boas vindas
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem de conta criada
     }
 }
